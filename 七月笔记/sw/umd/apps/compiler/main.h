45f2f4f98c72e065a41ccd27e5ade364fe319195
Rafael <63440757+Eloncoder@users.noreply.github.com>
2022-07-19 10:59:15 +0800

    Update main.h

--- 七月笔记/sw/umd/apps/compiler/main.h
+++ 七月笔记/sw/umd/apps/compiler/main.h
@@ -26,8 +26,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "dlaerror.h"
-#include "dlatypes.h"
+#include "dlaerror.h"     // sw/umd/core/include/dlaerror.h
+#include "dlatypes.h"      // sw/umd/core/include/dlatypes.h
 
 #include "nvdla/ILoadable.h"
 #include "nvdla/IRuntime.h"
